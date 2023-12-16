85bb6b828953d92a05bb211ce45e15b0f9115263
hali241997 <mhali241997@gmail.com>
2023-12-16 23:26:23 +0500

    Fix spacing

--- src/components/AppContainer/index.tsx
+++ src/components/AppContainer/index.tsx
@@ -1,35 +1,18 @@
-"use client";
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC } from "react";
 import BottomNav from "../BottomNav";
 import TopNav from "../TopNav";
-import { useWindowSize } from "usehooks-ts";
 
 export interface AppContainerProps {
   children: React.ReactNode;
 }
 
 const AppContainer: FC<AppContainerProps> = ({ children }) => {
-  const { height: windowHeight } = useWindowSize();
-
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (ref.current) {
-      const { height } = ref.current.getBoundingClientRect();
-      if (height < windowHeight) {
-        // console.log("move bottomNav to bottom");
-      } else {
-        // console.log("bottom nav should be inline");
-      }
-    }
-  }, [windowHeight]);
-
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <TopNav />
-      <div ref={ref}>{children}</div>
+      <div className="flex flex-1 flex-col">{children}</div>
       <BottomNav />
-    </>
+    </div>
   );
 };
 
