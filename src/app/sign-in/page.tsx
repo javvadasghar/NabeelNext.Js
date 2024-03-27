9fa485396d381f468613f677237049e49bd1b112
javvadasghar <muhammad-jawad.asghar@tu-ilmenau.de>
2024-03-27 22:42:32 +0100

    sign in,sign up api integration, forgot password ui and api integrate

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,35 +1,139 @@
+"use client";
 import TextInput from "@/components/Form/TextInput";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
+import { useState } from "react";
 import { FC } from "react";
 
 const SignIn: FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const SignInPostApi = "http://iwiygi-dev-server-env.eba-tsczssg5.us-east-1.elasticbeanstalk.com/api/auth/signin"
+    try {
+      const response = await fetch(SignInPostApi, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to sign in');
+      }
+    } catch (error) {
+      setError('Failed to sign in. Please try again.');
+    }
+  };
+
+  const handleForgotPasswordClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleForgotPasswordSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      debugger;
+      const response = await fetch(
+        "http://iwiygi-dev-server-env.eba-tsczssg5.us-east-1.elasticbeanstalk.com/api/auth/forgotPassword",
+        {
+          method: "POST",
+          mode: 'cors',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }),
+        }
+      );
+      debugger;
+      if (!response.ok) {
+        debugger;
+        throw new Error("Failed to submit email for password reset.");
+      }
+      // If successful, you might want to show a success message to the user
+      console.log("Email submitted successfully:", email);
+      // Close the modal after submission
+      // handleCloseModal();
+    } catch (error) {
+      console.error("Error submitting email for password reset:", error);
+      // Handle error gracefully, show error message to the user
+    }
+  };
+
   return (
     <div className="p-4 flex flex-col gap-[100px]">
       <div className="bg-black border-2 border-bright-green p-2">
+      <form onSubmit={handleSubmit}>
         <div className="text-center text-[30px] font-bold italic tracking-wider mb-4">
           - MEMBER SIGN IN -
         </div>
 
         <div className="flex gap-4 mb-6">
-          <TextInput label="USERNAME:" className="w-full" />
-          <TextInput label="PASSWORD:" type="password" className="w-full" />
+          <TextInput label="USERNAME:"  value={username} onChange={(e) => setUsername(e.target.value)}   className="w-full" />
+          <TextInput label="PASSWORD:" type="password" value={password}  onChange={(e) => setPassword(e.target.value)}   className="w-full" />
         </div>
 
         <div className="flex justify-center gap-8 mb-6">
           <div className="flex justify-center">
-            <div className="bg-dark-green text-black rounded-[50%] px-[50px] py-[10px] font-bold text-[24px] italic">
+            <button type="submit" className="bg-dark-green text-black rounded-[50%] px-[50px] py-[10px] font-bold text-[24px] italic">
               Sign in
-            </div>
+            </button>
           </div>
 
           <div className="flex justify-center">
-            <div className="bg-dark-green text-black rounded-[50%] px-[50px] py-[10px] font-bold text-[24px] italic">
+            <button className="bg-dark-green text-black rounded-[50%] px-[50px] py-[10px] font-bold text-[24px] italic"
+            onClick={handleForgotPasswordClick}>
               Forgot Your Password or Username?
-            </div>
+            </button>
           </div>
         </div>
+        </form>
       </div>
+
+       {/* Modal */}
+       {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+          <div className="bg-black border-2 border-bright-green p-2">
+            <h2 className="text-2xl font-bold mb-4">Forgot Password</h2>
+            <form onSubmit={handleForgotPasswordSubmit}>
+              <TextInput
+                label="Enter your email:"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <div className="mt-4 gap-4 flex justify-end">
+                <button
+                  type="button"
+                  className="bg-dark-green text-black rounded-[50%] px-[50px] py-[10px] font-bold text-[24px] italic"
+                  onClick={handleCloseModal}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="bg-dark-green text-black rounded-[50%] px-[50px] py-[10px] font-bold text-[24px] italic"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
 
       <Link href="/sign-up">
         <div className="flex justify-center mb-[100px]">
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,26 +1,64 @@
+"use client";
 import TextInput from "@/components/Form/TextInput";
 import Image from "next/image";
-import { FC } from "react";
+import axios from "axios";
+import { FC, useState } from "react";
 
 const SignUp: FC = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    city: "",
+    state: "",
+    zip: "",
+    mailingAddress: ""
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const SignUpPostApi = "http://iwiygi-dev-server-env.eba-tsczssg5.us-east-1.elasticbeanstalk.com/api/auth/signup";
+    debugger;
+    try {
+      await axios.post(SignUpPostApi, formData
+    ).then(response => {
+    });} catch (error) {
+      console.error("Error while signing up:");
+    }
+  };
   return (
     <div className="p-4 ">
       <div className="bg-black border-2 border-bright-green p-2">
         <div className="text-center text-[30px] font-bold italic tracking-wider mb-4">
           - CREATE YOUR NEW ACCOUNT -
         </div>
-
+        <form onSubmit={handleSubmit}>
         <div className="flex flex-col gap-6">
-          <TextInput label="USERNAME:" />
-          <TextInput label="PASSWORD:" type="password" />
-          <TextInput label="RETYPE PASSWORD:" type="password" />
-          <TextInput label="EMAIL:" type="email" />
-          <TextInput label="MAILING ADDRESS:" />
+          <TextInput name="username" label="USERNAME:"  value={formData.username}
+              onChange={handleChange}/>
+          <TextInput label="PASSWORD:" name="password"  value={formData.password}
+              onChange={handleChange} type="password" />
+          <TextInput label="RETYPE PASSWORD:" name="retypepassword"  
+          // value={formData.retypepassword}
+              onChange={handleChange} type="password" />
+          <TextInput label="EMAIL:" name="email" value={formData.email}
+              onChange={handleChange} type="email" />
+          <TextInput label="MAILING ADDRESS:" name="mailingAddress"  value={formData.mailingAddress}
+              onChange={handleChange}/>
 
           <div className="flex gap-4 pr-6">
-            <TextInput label="CITY:" className="w-full" />
-            <TextInput label="STATE:" className="w-full" />
-            <TextInput label="ZIP:" className="w-full" />
+            <TextInput label="CITY:" name="city" value={formData.city}
+              onChange={handleChange} className="w-full" />
+            <TextInput label="STATE:" name="state" value={formData.state}
+              onChange={handleChange} className="w-full" />
+            <TextInput label="ZIP:" name="zip"  value={formData.zip}
+              onChange={handleChange} className="w-full" />
           </div>
 
           <div className="flex justify-center pt-[100px] pb-[50px]">
@@ -28,9 +66,9 @@ const SignUp: FC = () => {
               <div className="italic text-[24px] font-bold">
                 Click Here to finish
               </div>
-              <div className="italic text-[24px] font-bold">
+              <button type="submit" className="italic text-[24px] font-bold">
                 creating you New Account
-              </div>
+              </button>
 
               <Image
                 src="/images/underline.png"
@@ -41,6 +79,7 @@ const SignUp: FC = () => {
             </div>
           </div>
         </div>
+        </form>
       </div>
     </div>
   );
